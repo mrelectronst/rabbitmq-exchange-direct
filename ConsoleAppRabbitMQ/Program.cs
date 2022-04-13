7e6aaacd7a2994e04a684e839f5fb65484cdb53c
mrelectronst <fhkaraman@outlook.com>
2022-04-13 16:49:02 +0300

    Add project files.

---
+++ ConsoleAppRabbitMQ/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using PublisherRabbitMQ;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rktcczxq:eBER7Ivsuu2oNFAD-RPxpEFEPZMu99W5@whale.rmq.cloudamqp.com/rktcczxq"); //write AMQP URL
+
+string messageRead;
+
+do
+{
+    Console.Write("Please write your message for 50 times: ");
+    messageRead = Console.ReadLine();
+    if (messageRead != string.Empty && messageRead != null)
+    {
+        PublishMessage(messageRead);
+    }
+    else
+    {
+        Environment.Exit(0);
+    }
+
+} while (messageRead != null);
+
+void PublishMessage(string message)
+{
+    try
+    {
+        using (var connection = factory.CreateConnection())
+        {
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("exchange-direct-logs", durable: true, type: ExchangeType.Direct);
+
+            Enum.GetNames(typeof(LogNames.EnumLognames)).ToList().ForEach(x =>
+            {
+                var queueName = $"direct-queue-{x}";
+                channel.QueueDeclare(queueName, durable: true, false, false);
+
+                var routeKey = $"route-{x}";
+
+                channel.QueueBind(queueName, "exchange-direct-logs", routeKey, null);
+            });
+
+            Enumerable.Range(0, 40).ToList().ForEach(x =>
+            {
+                LogNames.EnumLognames enumLognames = (LogNames.EnumLognames)new Random().Next(1, 5);
+
+                var messageBody = Encoding.UTF8.GetBytes(message + $"_{enumLognames}");
+
+                var routeKey = $"route-{enumLognames}";
+
+                channel.BasicPublish("exchange-direct-logs", routeKey, null, messageBody);
+
+                Console.WriteLine($"'{message}-{enumLognames}' is sended");
+            });
+
+            Console.WriteLine("All Logs are sended");
+        }
+    }
+    catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+
+
+}
+
+
---
+++ SubscriberRabbitMQ/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://rktcczxq:eBER7Ivsuu2oNFAD-RPxpEFEPZMu99W5@whale.rmq.cloudamqp.com/rktcczxq"); //write AMQP URL
+
+using (var connection = factory.CreateConnection())
+{
+    var channel = connection.CreateModel();
+
+    channel.BasicQos(0, 1, false);
+
+    var subscriber = new EventingBasicConsumer(channel);
+
+    string queueName = "direct-queue-Critical";
+
+    channel.BasicConsume(queueName, false, subscriber);
+
+    Console.WriteLine("Listening...");
+
+    subscriber.Received += (object? sender, BasicDeliverEventArgs e) =>
+    {
+        var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+        Thread.Sleep(1500);
+
+        Console.WriteLine($"Received Message : {message}");
+
+        //File.AppendAllText("logs"+ queueName + ".txt", message + "\n");
+
+        channel.BasicAck(e.DeliveryTag, false);
+    };
+
+    Console.ReadLine();
+}
